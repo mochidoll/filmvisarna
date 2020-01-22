4cfba2e9e3329f47ab213fd1d5530ab1881b1b3f
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-01-22 15:33:53 +0100

    Fixed picture resizing
    Malena och Heidi

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -7,11 +7,11 @@
     <div class="row">
 
       <div class="left col s12 center flex">
-        <h3 class="left">{{movie.title}} </h3>
-        <span class="movie-year">({{movie.productionYear}})</span>
+        <h3 class="left">{{movie.title}} <span class="movie-year">({{movie.productionYear}})</span></h3>
+        
       </div>
 
-        <div class="col s12 m4 center image">
+        <div class="col s12 m6 center image">
           <img :src="movie.image" alt="Movie poster" />
         </div>
         <div class="left col s12 m6 right">
@@ -56,17 +56,21 @@ export default {
   box-sizing: border-box;
 }
 h3 {
-  margin: 0rem !important
+  margin: 0rem !important;
+  padding-bottom: 2%;
+}
+h4{
+  margin-top: 0 !important;
 }
 .movie-year {
   font-size: 2rem;
+  display: inline-block;
+  position: relative;
+  top: -4px;
 }
-.flex {
-  display: flex;
-  align-items: center;
-   margin-bottom: 1rem;
-}
-.image{
-min-width: 356px;
+
+.image img{
+width: 100%;
+padding-right: 10% !important;
 }
 </style>
