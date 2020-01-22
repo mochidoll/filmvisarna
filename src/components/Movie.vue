e827571df61d33b946f32b11a57b7148fd482c27
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-01-22 11:51:10 +0100

    Movie.title etc can now be used in Movie component

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -4,15 +4,23 @@
     <h1>Insert trailer here later</h1>
   </div>
   <div class="container">
+      <p>{{movie.title}}</p>
+    </div>
 
-
-  </div>
 </div>
 </template>
 
 <script>
 export default {
   computed: {
+    movie(){
+      let movies = this.movies;
+      for (let movie of movies){
+        if (movie.title == this.$route.params.movie)
+        return movie;
+      }
+      return null
+    },
     movies() {
       return this.$store.state.movies
     }
