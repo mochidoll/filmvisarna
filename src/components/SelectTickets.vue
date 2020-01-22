3b2a76033a413329c77968948f85853ac57d99a9
wellbry <m_wellbring@yahoo.com>
2020-01-22 10:50:17 +0100

    Select tickets bare functionality

--- src/components/SelectTickets.vue
+++ src/components/SelectTickets.vue
@@ -1,14 +1,42 @@
 <template>
   <div class="container">
     <h2>Select Tickets</h2>
-    <p>
-      Adult Tickets
-      <a class="btn-floating" @click="addAdultTicket()">-</a>
-      {{tickets}}
-      <a class="btn-floating">+</a>
-    </p>
-    <p></p>
-    <p></p>
+
+    <div class="row">
+      <div class="col s4 offset-s2">Adult Tickets</div>
+      <div class="col s4">
+        <a class="btn-floating">
+          <i class="material-icons red" @click="removeAdultTicket()">remove</i>
+        </a>
+        {{adultTickets}}
+        <a class="btn-floating" @click="addAdultTicket()">
+          <i class="material-icons red">add</i>
+        </a>
+      </div>
+
+      <div class="col s4 offset-s2">Child Tickets</div>
+      <div class="col s4">
+        <a class="btn-floating">
+          <i class="material-icons red" @click="removeChildTicket()">remove</i>
+        </a>
+        {{childTickets}}
+        <a class="btn-floating" @click="addChildTicket()">
+          <i class="material-icons red">add</i>
+        </a>
+      </div>
+
+      <div class="col s4 offset-s2">Adult Tickets</div>
+      <div class="col s4">
+        <a class="btn-floating">
+          <i class="material-icons red" @click="removeSeniorTicket()">remove</i>
+        </a>
+        {{seniorTickets}}
+        <a class="btn-floating" @click="addSeniorTicket()">
+          <i class="material-icons red">add</i>
+        </a>
+      </div>
+      <div class="col s12">Total Price: {{totalTicketPrice}}</div>
+    </div>
   </div>
 </template>
 
@@ -16,14 +44,37 @@
 export default {
   data() {
     return {
-      tickets: 0,
+      adultTickets: 0,
       seniorTickets: 0,
-      childTickets: 0
-    }
+      childTickets: 0,
+      adultTicketPrice: 85,
+      seniorTicketPrice: 75,
+      childTicketPrice: 65,
+    };
   },
   methods: {
     addAdultTicket() {
-      this.tickets++;
+      this.adultTickets++;
+    },
+    removeAdultTicket() {
+      this.adultTickets--;
+    },
+    addChildTicket() {
+      this.childTickets++;
+    },
+    removeChildTicket() {
+      this.childTickets--;
+    },
+    addSeniorTicket() {
+      this.seniorTickets++;
+    },
+    removeSeniorTicket() {
+      this.seniorTickets--;
+    }
+  },
+  computed: {
+    totalTicketPrice() {
+      return this.adultTickets*this.adultTicketPrice + this.childTickets*this.childTicketPrice + this.seniorTickets*this.seniorTicketPrice;
     }
   },
 };
