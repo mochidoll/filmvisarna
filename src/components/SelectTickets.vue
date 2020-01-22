6da3c9c5adcfb75bb7861bec8137553fd9a545a2
wellbry <m_wellbring@yahoo.com>
2020-01-22 13:36:42 +0100

    Can no longer select a negative amount of tickets

--- src/components/SelectTickets.vue
+++ src/components/SelectTickets.vue
@@ -1,41 +1,50 @@
 <template>
   <div class="container">
-    <h2>Select Tickets</h2>
+    <h2 class="center">Select Tickets</h2>
 
     <div class="row">
-      <div class="col s6">Adult Tickets</div>
+      <div class="col s6 right-align">Adult Tickets</div>
       <div class="col s6">
-        <a class="btn-floating">
-          <i class="material-icons red" @click="removeAdultTicket()">remove</i>
+        <a class="btn-floating red disabled" v-if="adultTickets == 0">
+          <i class="material-icons">remove</i>
+        </a>
+        <a class="btn-floating red" v-else @click="removeAdultTicket()">
+          <i class="material-icons">remove</i>
         </a>
         {{adultTickets}}
-        <a class="btn-floating" @click="addAdultTicket()">
-          <i class="material-icons red">add</i>
+        <a class="btn-floating red" @click="addAdultTicket()">
+          <i class="material-icons">add</i>
         </a>
       </div>
 
-      <div class="col s6">Child Tickets</div>
+      <div class="col s6 right-align">Child Tickets</div>
       <div class="col s6">
-        <a class="btn-floating">
-          <i class="material-icons red" @click="removeChildTicket()">remove</i>
+        <a class="btn-floating red disabled" v-if="childTickets == 0">
+          <i class="material-icons">remove</i>
+        </a>
+        <a class="btn-floating red" v-else @click="removeChildTicket()">
+          <i class="material-icons">remove</i>
         </a>
         {{childTickets}}
-        <a class="btn-floating" @click="addChildTicket()">
-          <i class="material-icons red">add</i>
+        <a class="btn-floating red" @click="addChildTicket()">
+          <i class="material-icons">add</i>
         </a>
       </div>
 
-      <div class="col s6">Senior Tickets</div>
+      <div class="col s6 right-align">Senior Tickets</div>
       <div class="col s6">
-        <a class="btn-floating">
-          <i class="material-icons red" @click="removeSeniorTicket()">remove</i>
+        <a class="btn-floating red disabled" v-if="seniorTickets == 0">
+          <i class="material-icons">remove</i>
+        </a>
+        <a class="btn-floating red" v-else @click="removeSeniorTicket()">
+          <i class="material-icons">remove</i>
         </a>
         {{seniorTickets}}
-        <a class="btn-floating" @click="addSeniorTicket()">
-          <i class="material-icons red">add</i>
+        <a class="btn-floating red" @click="addSeniorTicket()">
+          <i class="material-icons">add</i>
         </a>
       </div>
-      <div class="col s12">Total Price: {{totalTicketPrice}}</div>
+      <div class="col s12 center">Total Price: {{totalTicketPrice}}</div>
     </div>
   </div>
 </template>
