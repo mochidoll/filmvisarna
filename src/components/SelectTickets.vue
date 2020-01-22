abeb647aac147dd4c0ba5d2b784f389462f0587e
wellbry <m_wellbring@yahoo.com>
2020-01-22 10:10:35 +0100

    Auto stash before merge of "feature/booking" and "origin/feature/booking"

--- src/components/SelectTickets.vue
+++ src/components/SelectTickets.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="container">
- 
-      <h2>Select seats</h2>
-  
-    <button class="btn waves-effect waves-light">Change Active</button>
+    <h2>Select Tickets</h2>
+    <p>
+      Adult Tickets
+      <a class="btn-floating" @click="addAdultTicket()">-</a>
+      {{tickets}}
+      <a class="btn-floating">+</a>
+    </p>
+    <p></p>
+    <p></p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tickets: 0,
+      seniorTickets: 0,
+      childTickets: 0
+    };
+  },
+  methods() {
+    addAdultTicket() {
+      tickets++;
+    }
+  },
+};
 </script>
 
 <style scoped>
-.navbar {
-  padding: 10px;
-}
-.container {
-  padding: 10px;
-}
 </style>
