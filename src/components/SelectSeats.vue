54ea3fb5bfb49486ed42ddfb9e3d2b77134b410b
DanielHansson <dh99@live.se>
2020-01-23 12:31:34 +0100

    Auto stash before merge of "feature/booking" and "origin/feature/booking"

--- src/components/SelectSeats.vue
+++ src/components/SelectSeats.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="container">
-  
       <h4>Select seats</h4>
-      
-
-
-
+    <button class="btn waves-effect waves-light black white-text right">Next</button>
+    <div class="row">
+      <div class="col green" v-for="(row, id) in seatsPerRow" :key=id>
+        <p>{{row}}</p>
+      </div>
+    </div>
       <button class="btn waves-effect waves-light black white-text right">Next</button>
-    
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      seatsPerRow: 10//this.$store.state.auditoriums[0].seatsPerRow
+    }
+  },
+  methods: {
+    
+  }
+};
 </script>
 
 <style scoped>
