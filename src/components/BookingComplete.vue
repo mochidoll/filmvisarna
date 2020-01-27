6f016a99dd3192df70cb0f3a8386e185b2e5c6f7
DanielHansson <dh99@live.se>
2020-01-27 08:02:07 +0100

    Rough init of bookingcomplete

--- src/components/BookingComplete.vue
+++ src/components/BookingComplete.vue
@@ -1,13 +1,41 @@
 <template>
-  <div></div>
+  <div>
+    <h2>Booking Complete!</h2>
+    <!--Loops through all data inside user, if we want to add more or less information in an user -->
+    <ul v-for="(user, id) in users" :key="'user' + id">
+      <BookingInformation :data="user"></BookingInformation>
+    </ul>
+    <h2>Thanks for booking!</h2>
+  </div>
 </template>
 
 <script>
-export default {
+import BookingInformation from "@/components/BookingInformation";
 
-}
+export default {
+  components: {
+    BookingInformation
+  },
+  data() {
+    return {
+      users: [
+        {
+          userId: 20,
+          movie: "Random film of greatest random times",
+          tickets: {
+            children: 0,
+            adults: 1,
+            seniors: 0
+          },
+          seats: [2],
+          price: 85,
+          date: "2020-03-02 18:00"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/BookingInformation.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h4>Price: {{data.price}} SEK</h4>
+    <h5>
+      UserId: {{data.userId}}
+      <br />
+      Movie: {{data.movie}}
+      <br />
+      Tickets:
+      {{data.tickets.children}} children,
+      {{data.tickets.adults}} adults,
+      {{data.tickets.seniors}} seniors
+      <br />
+      date: {{data.date}}
+    </h5>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["data"],
+  data() {
+    return {
+      seatNumbers: String
+    };
+  }
+};
+</script>
+
+<style>
+</style>
