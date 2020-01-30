96b6744164981707859325d08d64807d89352a9a
Paola <paolatosca@outlook.com>
2020-01-30 12:58:18 +0100

    refacturing

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,64 +1,69 @@
 <template>
-<div class="container">
-  <div class="trailer-container">
-    <h4 class="grey-text center">Insert trailer here later</h4>
-  </div>
+  <div class="container">
+    <div class="trailer-container">
+      <h4 class="grey-text center">Insert trailer here later</h4>
+    </div>
 
     <div class="row">
-
       <div class="left col s12 center flex">
-        <h3 class="left">{{movie.title}} <span class="movie-year">({{movie.productionYear}})</span></h3>
-        
+        <h3 class="left">
+          {{movie.title}}
+          <span class="movie-year">({{movie.productionYear}})</span>
+        </h3>
       </div>
 
-        <div class="col s12 m6 center image">
-          <img :src="movie.image" alt="Movie poster" />
-        </div>
-        <div class="left col s12 m6 right">
-          <h4>Summary</h4>
-          <p class="grey-text">{{movie.length}} minutes</p>
-          <p>{{movie.description}}</p>
-        </div>
-        <div class="col s12 m6 right">
-          <span>Actors: {{movie.actors.join(", ")}}</span>
-        </div>
-        <div class="col s12 m6 right">
-          <span>Directors: {{movie.directors.join(", ")}}
-          </span>
-        </div>
-        <div class="col s12 m6 right">
-          <span>Genre: {{movie.genre.join(", ")}}</span>
-         
-        </div>
+      <div class="col s12 m6 center image">
+        <img :src="movie.image" alt="Movie poster" />
+      </div>
+      <div class="left col s12 m6 right">
+        <h4>Summary</h4>
+        <p class="grey-text">{{movie.length}} minutes</p>
+        <p>{{movie.description}}</p>
+      </div>
+      <div class="col s12 m6 right">
+        <span>Actors: {{movie.actors.join(", ")}}</span>
+      </div>
+      <div class="col s12 m6 right">
+        <span>Directors: {{movie.directors.join(", ")}}</span>
+      </div>
+      <div class="col s12 m6 right">
+        <span>Genre: {{movie.genre.join(", ")}}</span>
+      </div>
 
-        <dropdown :options="arrayOfObjects" :selected="object" v-on:updateOption="methodToRunOnSelect"></dropdown>
-
+      <dropdown
+        :options="viewingDates"
+        class="options"
+        :selected="chosenDate"
+        v-on:updateOption="updateChosenDate"
+        :placeholder="'Select an Item'"
+      ></dropdown>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-
-import dropdown from 'vue-dropdowns';
+import dropdown from "vue-dropdowns";
 
 export default {
   data() {
-          return {
-            arrayOfObjects: [],
-            object: {
-              name: 'Choose Date',
-            }
-          }
-        },
-            components: {
-            'dropdown': dropdown,
-        },
+    return {
+      viewingDates: [
+        { name: "23-09-2010"},
+      ],
+      chosenDate: {
+        name: "Choose Date"
+      }
+    };
+  },
+  components: {
+    dropdown: dropdown
+  },
 
-        methods: {
-          methodToRunOnSelect(payload) {
-            this.object = payload;
-          }
-        },
+  methods: {
+    updateChosenDate(date) {
+      this.chosenDate = date;
+    }
+  },
   computed: {
     movie() {
       let movies = this.movies;
@@ -82,7 +87,7 @@ h3 {
   margin: 0rem !important;
   padding-bottom: 2%;
 }
-h4{
+h4 {
   margin-top: 0 !important;
 }
 .movie-year {
@@ -92,8 +97,11 @@ h4{
   top: -4px;
 }
 
-.image img{
-width: 100%;
-padding-right: 10% !important;
+.image img {
+  width: 100%;
+  padding-right: 10% !important;
+}
+.options {
+  color: blue;
 }
 </style>
