d4bd7df2ff76e8ebd95b060753f66734218ccd8d
Magnus <m_wellbring@yahoo.com>
2020-02-02 18:10:59 +0100

    Added sign up to login page, added v-model hooks but not functionality

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,59 @@
 <template>
   <div class="container center z-depth-1">
-<div class="row">
-  <h4 class="">Log in</h4>
-  <div class="col s12">
-    <div class="input-field">
-      <input type="text" id="login" class="active" v-model="username" />
-      <label for="login">Login</label>
-    </div>
+    <div class="row">
+      <div class="row col s12 m6">
+        <h4 class>Log in</h4>
+        <div class="col s12">
+          <div class="input-field">
+            <input type="text" id="login" class="active" v-model="username" />
+            <label for="login">Login</label>
+          </div>
 
-    <div class="input-field">
-      <label for="password">Password</label>
-      <input type="password" id="password" class="active" v-model="password" />
+          <div class="input-field">
+            <label for="password">Password</label>
+            <input type="password" id="password" class="active" v-model="password" />
+          </div>
+        </div>
+        <div class="col s12 left-align">
+          <a
+            class="btn waves-effect waves-light red darken-4"
+            v-if="loggedInUser"
+            @click="login()"
+          >Log In</a>
+          <a
+            class="btn waves-effect waves-light red darken-4"
+            v-else
+            @click="logOut()"
+          >Log Out</a>
+        </div>
+      </div>
+      <div class="row col s12 m6">
+        <h4>Sign Up</h4>
+        <div class="col s12 l6">
+          <div class="input-field">
+            <input type="text" id="signUpFirstName" v-model="signUpFirstName" class="active" />
+            <label for="signUpFirstName">First Name</label>
+          </div>
+        </div>
+        <div class="col s12 l6">
+          <div class="input-field">
+            <input type="text" id="signUpLastName" v-model="signUpLastName" class="active" />
+            <label for="signUpLastName">Last Name</label>
+          </div>
+        </div>
+        <div class="input-field col s12">
+          <input type="text" id="signUpEmail" v-model="signUpEmail" class="active" />
+          <label for="signUpEmail">Email</label>
+        </div>
+        <div class="input-field col s12">
+          <input type="password" id="signUpPassword" v-model="signUpPassword" class="active" />
+          <label for="signUpPassword">Password</label>
+        </div>
+        <div class="col s12 left-align">
+          <a class="btn waves-effect waves-light red darken-4" @click="signUp()">Sign Up</a>
+        </div>
+      </div>
     </div>
-</div>
-<div class="col s12">
-    <a class="btn waves-effect waves-light red darken-4" v-if="loggedInUser" @click="login()">Log In</a>
-    <a class="btn waves-effect waves-light red darken-4" v-else @click="logOut()">Log Out</a>
-</div>
-</div>
   </div>
 </template>
 
@@ -27,6 +63,10 @@ export default {
     return {
       username: "",
       password: "",
+      signUpFirstName: "",
+      signUpLastName: "",
+      signUpEmail: "",
+      signUpPassword: "",
     };
   },
   methods: {
@@ -35,28 +75,33 @@ export default {
         return user.userName === this.username;
       })[0];
       //window.console.log(attemptUser);
-      window.console.log(this.username)
+      window.console.log(this.username);
 
-      if (this.username.toLowerCase() === attemptUser.userName.toLowerCase() && this.password === attemptUser.password) {
-
+      if (
+        this.username.toLowerCase() === attemptUser.userName.toLowerCase() &&
+        this.password === attemptUser.password
+      ) {
         window.console.log("Correct");
         window.console.log(this.username);
         this.$store.commit("setLoggedInUser", attemptUser);
         this.username = "";
         this.password = "";
-        this.$router.push('secure')
-       // window.console.log(this.loggedInUser);
+        this.$router.push("secure");
+        // window.console.log(this.loggedInUser);
       } else {
         window.console.log("Fail");
         this.password = "";
       }
       window.console.log(this.loggedInUser);
       if (this.loggedInUser) {
-        window.console.log('hej')
+        window.console.log("hej");
       }
     },
     logOut() {
       this.$store.commit("setLoggedInUser", null);
+    },
+    signUp() {
+      window.console.log(this.signUpFirstName + " " + this.signUpLastName)
     }
   },
   computed: {
@@ -83,9 +128,8 @@ input[type="password"]:focus {
 label {
   color: #3f3f3f !important;
 }
-.container{
+.container {
   margin-top: 2rem;
   margin-bottom: 2rem;
-
 }
 </style>
