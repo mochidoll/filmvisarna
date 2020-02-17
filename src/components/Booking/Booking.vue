4b6a0182b915dad66c5c6ff09ed9f21703d26624
wellbry <m_wellbring@yahoo.com>
2020-02-17 15:43:45 +0100

    UGLY fix fix

--- src/components/Booking/Booking.vue
+++ src/components/Booking/Booking.vue
@@ -1,31 +1,55 @@
 <template>
   <div class="container center">
-    <div class="row z-depth-1 white">
+    <div class="row z-depth-1 white" v-if="loadTicketContent">
       <div class="booking nav-wrapper">
-
         <div v-if="!showErrorText" class="row nav-choices valign-wrapper">
-
-          <div @click="back" v-if="showOrNot" class="row col s2 m4 nav-buttons backwards left-align valign-wrapper">
+          <div
+            @click="back"
+            v-if="showOrNot"
+            class="row col s2 m4 nav-buttons backwards left-align valign-wrapper"
+          >
             <i class="col s2 material-icons left-align">arrow_back_ios</i>
             <h6 class="col s10 hide-on-small-only">{{ before }}</h6>
           </div>
 
           <h4 v-if="showOrNot" class="col s8 m4 nav-text center">{{navText}}</h4>
 
-          <div @click="forward" v-if="showOrNot" class="row col s2 m4 nav-buttons forward right-align">
+          <div
+            @click="forward"
+            v-if="showOrNot"
+            class="row col s2 m4 nav-buttons forward right-align"
+          >
             <h6 class="col s10 hide-on-small-only">{{ after }}</h6>
             <i class="col s2 material-icons">arrow_forward_ios</i>
           </div>
-
         </div>
 
         <div v-if="showErrorText" class="row nav-choices valign-wrapper">
           <h5 class="col s12 nav-text red-text">{{ errorText }}</h5>
         </div>
-        
+
         <div v-if="showOrNot" class="divider"></div>
       </div>
-      <router-view @toggleErrorText="toggleErrorText" ref="contentView" @changeNavText="changeNavText"></router-view>
+      <router-view
+        @toggleErrorText="toggleErrorText"
+        ref="contentView"
+        @changeNavText="changeNavText"
+      ></router-view>
+    </div>
+    <div v-else class="valign-wrapper">
+      <div class="preloader-wrapper big active">
+        <div class="spinner-layer spinner-red-only">
+          <div class="circle-clipper left">
+            <div class="circle"></div>
+          </div>
+          <div class="gap-patch">
+            <div class="circle"></div>
+          </div>
+          <div class="circle-clipper right">
+            <div class="circle"></div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -42,107 +66,113 @@ export default {
       navText: null,
       feedback: null,
       showErrorText: false,
-      errorText: null,
-    }
+      errorText: null
+    };
   },
 
   methods: {
-
     toggleErrorText(payload) {
-
-      switch(payload.component) {
-        
-        case 1: 
-          this.errorText = 'Du måste välja minst en biljett..'
-          this.showErrorText = true
+      switch (payload.component) {
+        case 1:
+          this.errorText = "Du måste välja minst en biljett..";
+          this.showErrorText = true;
           break;
         case 2:
-          this.errorText = `Du måste välja minst ${payload.numberOfTickets} plats(er)..`
-          this.showErrorText = true
+          this.errorText = `Du måste välja minst ${payload.numberOfTickets} plats(er)..`;
+          this.showErrorText = true;
           break;
         case 3:
-          this.errorText = 'Du måste skriva in en giltig email..'
-          this.showErrorText = true
+          this.errorText = "Du måste skriva in en giltig email..";
+          this.showErrorText = true;
           break;
       }
-      
-      setTimeout(() => {
-        this.showErrorText = false
-      },2000)
 
+      setTimeout(() => {
+        this.showErrorText = false;
+      }, 2000);
     },
-    
+
     changeNavText(value) {
-      this.navText = value
+      this.navText = value;
     },
 
     back() {
       switch (this.navText) {
         case this.$store.state.navTexts[1]:
-          this.$store.state.bookingObject.adultTickets = 0
-          this.$store.state.bookingObject.childTickets = 0
-          this.$store.state.bookingObject.seniorTickets = 0
-          this.$router.push({name: 'Home'})
-          break
+          this.$store.state.bookingObject.adultTickets = 0;
+          this.$store.state.bookingObject.childTickets = 0;
+          this.$store.state.bookingObject.seniorTickets = 0;
+          this.$router.push({ name: "Home" });
+          break;
         case this.$store.state.navTexts[2]:
-          this.$refs.contentView.goBackToSelectTickets()
-          break
+          this.$refs.contentView.goBackToSelectTickets();
+          break;
         case this.$store.state.navTexts[3]:
-          this.$refs.contentView.backToSelectSeats()
-          break
+          this.$refs.contentView.backToSelectSeats();
+          break;
       }
     },
 
     forward() {
       switch (this.navText) {
         case this.$store.state.navTexts[1]:
-          this.$refs.contentView.continueToSelectSeats()
-          break
+          this.$refs.contentView.continueToSelectSeats();
+          break;
         case this.$store.state.navTexts[2]:
-          this.$refs.contentView.goToConfirmDetails()
-          break
+          this.$refs.contentView.goToConfirmDetails();
+          break;
         case this.$store.state.navTexts[3]:
-          this.$refs.contentView.confirmBooking()
-          break
+          this.$refs.contentView.confirmBooking();
+          break;
       }
     }
   },
 
   computed: {
-
+    loadTicketContent() {
+      return (
+        this.$store.state.screenings[0] &&
+        this.$store.state.auditoriums[0] &&
+        this.$store.state.movies[0]
+      );
+    },
     showOrNot() {
-      return this.navText !== this.$store.state.navTexts[4]
+      return this.navText !== this.$store.state.navTexts[4];
     },
 
     after() {
-      let index = this.$store.state.navTexts.indexOf(this.navText)
+      let index = this.$store.state.navTexts.indexOf(this.navText);
 
-      if(index === 4) {
-        return this.$store.state.navTexts[index]
+      if (index === 4) {
+        return this.$store.state.navTexts[index];
       }
-        return this.$store.state.navTexts[index + 1]
+      return this.$store.state.navTexts[index + 1];
     },
 
     before() {
-      let index = this.$store.state.navTexts.indexOf(this.navText)
-      return this.$store.state.navTexts[index - 1]
+      let index = this.$store.state.navTexts.indexOf(this.navText);
+      return this.$store.state.navTexts[index - 1];
     }
   },
 
   beforeDestroy() {
-   let obj = {
+    let obj = {
       adultTickets: 0,
       seniorTickets: 0,
       childTickets: 0,
       screeningId: null
-   }
-   this.$store.commit('setBookingObject', obj)
-  },
-}
+    };
+    this.$store.commit("setBookingObject", obj);
+  }
+};
 </script>
 
 <style>
-.booking{
+.valign-wrapper {
+ height: 60vh;
+justify-content: center;
+}
+.booking {
   min-height: 80px;
 }
 .nav-choices {
